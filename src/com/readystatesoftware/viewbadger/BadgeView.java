71e71cfcad0815eb2daa3cff83cca64594ea3768
Jeff Gilfelt <jgilfelt@gmail.com>
2011-09-29 23:34:12 +0100

    implemented BadgeView api demos

--- src/com/readystatesoftware/viewbadger/BadgeView.java
+++ src/com/readystatesoftware/viewbadger/BadgeView.java
@@ -30,7 +30,7 @@ public class BadgeView extends TextView {
 	private static final int DEFAULT_MARGIN_DIP = 5;
 	private static final int DEFAULT_LR_PADDING_DIP = 5;
 	private static final int DEFAULT_CORNER_RADIUS_DIP = 8;
-	private static final int DEFAULT_POSITION = POSITION_BOTTOM_RIGHT;
+	private static final int DEFAULT_POSITION = POSITION_TOP_RIGHT;
 	private static final int DEFAULT_BADGE_COLOR = Color.RED;
 	private static final int DEFAULT_TEXT_COLOR = Color.WHITE;
 	
--- src/viewbadger/demo/DemoActivity.java
+++ src/viewbadger/demo/DemoActivity.java
@@ -1,6 +1,6 @@
 package viewbadger.demo;
 
-import android.app.Activity;
+import android.app.TabActivity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -8,69 +8,137 @@ import android.view.View.OnClickListener;
 import android.view.animation.BounceInterpolator;
 import android.view.animation.TranslateAnimation;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.TabHost;
 import android.widget.Toast;
 
 import com.readystatesoftware.viewbadger.BadgeView;
 import com.readystatesoftware.viewbadger.R;
 
-public class DemoActivity extends Activity {
+public class DemoActivity extends TabActivity {
     
-	ImageView icon;
-	TextView text;
-	Button button;
+	Button btnPosition;
+	Button btnColour;
+	Button btnAnim1;
+	Button btnAnim2;
+	Button btnCustom;
+	Button btnClick;
 	
-	BadgeView badge;
+	BadgeView badge1;
 	BadgeView badge2;
 	BadgeView badge3;
+	BadgeView badge4;
+	BadgeView badge5;
+	BadgeView badge6;
+	BadgeView badge7;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        icon = (ImageView) findViewById(R.id.imageView1);
-        text = (TextView) findViewById(R.id.textView1);
-        button = (Button) findViewById(R.id.apply);
         
-        // setup badges
+        final TabHost tabHost = getTabHost();
+
+        tabHost.addTab(tabHost.newTabSpec("demos")
+                .setIndicator("Badge Demos")
+                .setContent(R.id.tab1));
+
+        tabHost.addTab(tabHost.newTabSpec("tests")
+                .setIndicator("Layout Tests")
+                .setContent(R.id.tab2));
+       
+        // *** default badge ***
         
-        badge = new BadgeView(this, icon);
-        badge.setText("8");
-    	badge.setBadgeMargin(10);
-    	badge.setTextSize(12);
-    	badge.setBackgroundResource(R.drawable.badge_ifaux);
-    	
-    	badge2 = new BadgeView(this, text);
+		View target = findViewById(R.id.default_target);
+		BadgeView badge = new BadgeView(this, target);
+		badge.setText("1");
+		badge.show();
+        
+        // *** set position ***
+		
+        btnPosition = (Button) findViewById(R.id.position_target);
+        badge1 = new BadgeView(this, btnPosition);
+        badge1.setText("12");
+        badge1.setBadgePosition(BadgeView.POSITION_BOTTOM_LEFT);
+        btnPosition.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				badge1.toggle();
+			}
+		});
+        
+        // *** badge/text colour ***
+        
+    	btnColour = (Button) findViewById(R.id.colour_target);
+    	badge2 = new BadgeView(this, btnColour);
+    	badge2.setText("New!");
     	badge2.setTextColor(Color.BLUE);
     	badge2.setBadgeBackground(Color.YELLOW);
-    	badge2.setText("New!");
-    	badge2.setBadgePosition(BadgeView.POSITION_TOP_RIGHT);
-    	badge2.setOnClickListener(new OnClickListener() {
+    	btnColour.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				badge2.toggle();
+			}
+		});
+    	
+    	// *** default animation ***
+    	
+    	btnAnim1 = (Button) findViewById(R.id.anim1_target);
+        badge3 = new BadgeView(this, btnAnim1);
+        badge3.setText("84");
+        btnAnim1.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				badge3.toggle(true);
+			}
+		});
+    	
+        // *** custom animation ***
+        
+    	btnAnim2 = (Button) findViewById(R.id.anim2_target);
+        badge4 = new BadgeView(this, btnAnim2);
+    	badge4.setText("123");
+    	btnAnim2.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				TranslateAnimation anim = new TranslateAnimation(-100, 0, 0, 0);
+		        anim.setInterpolator(new BounceInterpolator());
+		        anim.setDuration(1000);
+		    	badge4.toggle(anim, null);
+			}
+		});
+        
+    	// *** custom background ***
+    	
+        btnCustom = (Button) findViewById(R.id.custom_target);
+        badge5 = new BadgeView(this, btnCustom);
+        badge5.setText("8");
+        badge5.setBackgroundResource(R.drawable.badge_ifaux);
+    	badge5.setBadgeMargin(10);
+    	badge5.setTextSize(12);
+    	btnCustom.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				badge5.toggle();
+			}
+		});
+    	
+    	// *** clickable badge ***
+    	
+    	btnClick = (Button) findViewById(R.id.click_target);
+    	badge6 = new BadgeView(this, btnClick);
+    	badge6.setText("click me");
+    	badge6.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				Toast.makeText(DemoActivity.this, "clicked badge", Toast.LENGTH_SHORT).show();
 			}
 		});
-    	
-    	badge3 = new BadgeView(this, button);
-    	badge3.setText("99");
-    	//badge3.setTextColor(Color.DKGRAY);
-    	//badge3.setTextSize(10);
-    	//badge3.setBackgroundColor(Color.WHITE);
-    	badge3.setBadgePosition(BadgeView.POSITION_TOP_LEFT);
-    	
-    }
-    
-    public void onApplyClick(View v) {
-    	
-    	TranslateAnimation anim = new TranslateAnimation(-100, 0, 0, 0);
-        anim.setInterpolator(new BounceInterpolator());
-        anim.setDuration(1000);
-    	
-    	badge.toggle(anim, null);
-    	badge2.toggle(true);
-    	badge3.toggle();
+    	btnClick.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				badge6.toggle();
+			}
+		});
     	
     }
     
